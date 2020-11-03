583f066167378f65e14cd3f15e4b8b5719470de1
Tim <timigntrt@gmail.com>
2020-11-03 14:01:12 +0300

    commit oldProj files

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import { App } from "./App"
+import * as serviceWorker from "./serviceWorker"
 // import * as connect from '@vkontakte/vkui-connect';
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux"
+import store from "./store"
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root")
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -15,9 +14,9 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   rootElement
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
